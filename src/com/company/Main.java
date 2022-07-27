e72bad89d992b6c0d75a5323f25a757d969d20dd
SubuserJava <telemarent@yandex.ru>
2022-07-28 00:34:49 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+
+        Scanner console = new Scanner(System.in);
+        String enterConsole = console.nextLine();
+
+        System.out.println(enterConsole);
+
+        char convertStringToChar = enterConsole.charAt(0);
+        System.out.println(convertStringToChar);
+        System.out.println();
+    }
+}
